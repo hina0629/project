e2a2064986d0c13e393740d6e02790b289e4bc36
hina0629 <hina.0629.xxx@gmail.com>
2025-09-23 13:26:13 +0900

    日本語で表示

--- node-vanilla/index.js
+++ node-vanilla/index.js
@@ -2,7 +2,8 @@
 const http = require("http")
 
 http.createServer(function(req, res) {
-    res.write("Hello")
+    res.setHeader("Content-type", "text/plain; charset=utf-8")
+    res.write("こんにちは")
     res.end()
 }).listen(4000, function() {
     console.log("Listening on localhost port 4000")
