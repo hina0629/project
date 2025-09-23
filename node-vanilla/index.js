04326f4a2ef4780787f9b7cf437e06e373e28499
hina0629 <hina.0629.xxx@gmail.com>
2025-09-23 13:33:08 +0900

    URLで表示を変える

--- node-vanilla/index.js
+++ node-vanilla/index.js
@@ -2,9 +2,16 @@
 const http = require("http")
 
 http.createServer(function(req, res) {
-    res.setHeader("Content-type", "text/plain; charset=utf-8")
-    res.write("こんにちは")
-    res.end()
+    if(req.url === "/"){
+        // console.log("reqの中身", req)
+        res.setHeader("Content-type", "text/plain; charset=utf-8")
+        res.write("こんにちは")
+        res.end()
+    }else if(req.url === "/about"){
+        res.setHeader("Content-type", "text/plain; charset=utf-8")
+        res.write("ここはAboutページです")
+        res.end()
+    }
 }).listen(4000, function() {
     console.log("Listening on localhost port 4000")
 })
